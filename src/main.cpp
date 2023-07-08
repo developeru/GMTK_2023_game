373bc4869c92ce7789a5236dbb8ee8b16422c4ad
Martim Neves <martim7435@gmail.com>
2023-07-08 15:05:39 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+/*******************************************************************************************
+ *
+ *   raylib [core] example - Basic window
+ *
+ *   Welcome to raylib!
+ *
+ *   To test examples, just press F6 and execute raylib_compile_execute script
+ *   Note that compiled executable is placed in the same folder as .c file
+ *
+ *   You can find all basic examples on C:\raylib\raylib\examples folder or
+ *   raylib official webpage: www.raylib.com
+ *
+ *   Enjoy using raylib. :)
+ *
+ *   Example originally created with raylib 1.0, last time updated with raylib 1.0
+ *
+ *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+ *   BSD-like license that allows static linking with closed source software
+ *
+ *   Copyright (c) 2013-2023 Ramon Santamaria (@raysan5)
+ *
+ ********************************************************************************************/
+
+#include <raylib.h>
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 640;
+    const int screenHeight = 640;
+
+    //Initializes Window
+    InitWindow(screenWidth, screenHeight, "Contra te ipsum - A chess game");
+
+    //Loads textures
+    Texture2D bg_image = LoadTexture("C:/Users/HP/Desktop/c++ projects/GMTK Game Jam 2023/src/Chess_images/chessboard.png");
+    // Sets our game to run at 60 frames-per-second
+    SetTargetFPS(60); 
+    
+    // Main game loop
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        ClearBackground(BLANK);
+        DrawTexture(bg_image, 0, 0, WHITE);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
